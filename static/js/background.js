81b0c2f5a9a1dd811d9707ea7a4e260c70fc4941
nilBora <brdnlsrg@gmail.com>
2017-04-17 23:03:30 +0300

    add tooltip

--- static/js/background.js
+++ static/js/background.js
@@ -1,5 +1,6 @@
 var APP = {
     remoteURL: "http://eng.word.local.com/get/translate/",
+	translateWord: 'null',
     init: function() {
        this.createContextMenu();
     },
@@ -19,28 +20,47 @@ var APP = {
 
     genericOnClick: function(info, tab) {
        // console.log(info);
-        console.log(tab);
-        chrome.tabs.executeScript( null,  {file: "/static/js/tooltip.js"});
+       // console.log(tab);
+       
 
-        console.log("item " + info.menuItemId + " was clicked");
-        console.log("info: " + JSON.stringify(info));
-        console.log("tab: " + JSON.stringify(tab));
+       // console.log("item " + info.menuItemId + " was clicked");
+        //console.log("info: " + JSON.stringify(info));
+       // console.log("tab: " + JSON.stringify(tab));
         //console.log(info['selectionText']);
-        APP.doSendTranslateAjax(info['selectionText']);
+		//console.log(APP.getSelectedText());
+        APP.doSendTranslateAjax(info['selectionText'], tab);
+		
+		
+		
     },
 
-    doSendTranslateAjax: function(string) {
+    doSendTranslateAjax: function(string, tab) {
         var xhr = new XMLHttpRequest();
         var params = "word="+string;
 
         jQuery.post(APP.remoteURL, {'word': string}, function(data) {
             var translateData = JSON.parse(data);
             var translateWord = translateData['content']['translate'];
-            console.log(translateWord);
+			APP.translateWord = translateWord;
+			
+			chrome.tabs.executeScript(tab.id, {
+				code: 'var translateWord = "'+translateWord+'";'
+			}, function() {
+				chrome.tabs.executeScript(tab.id, {file: '/static/js/tooltip.js'});
+			});
+			//chrome.tabs.executeScript(null, {file: "/static/js/tooltip.js"});
+			
+			//chrome.tabs.executeScript( null,  {file: "/static/js/tooltip.js"});
+            ///console.log(translateWord);
         });
 
 
-    }
+    },
+	getSelectedText: function(){
+		return (!!document.getSelection) ? document.getSelection() :
+           (!!window.getSelection)   ? window.getSelection() :
+           document.selection.createRange().text;
+	}
 }
 APP.init();
 
--- static/js/tooltip.js
+++ static/js/tooltip.js
@@ -1,1 +1,58 @@
 var div=document.createElement("div"); document.body.appendChild(div); div.innerText="test123";
+//var sel =  window.getSelection();
+//var range = sel.getRangeAt(0);
+
+//console.log(APP.translateWord);
+
+
+var positions =  getSelectionTopLeft();
+var tooltipDiv = document.createElement("div");
+var style = 'position:absolute;z-index:999;left:'+positions.x+'px;bottom:'+positions.y+'px; background-color:#dedede;padding:5px;border:1px solid #fff;width:250px;';
+tooltipDiv.setAttribute('style', style)
+document.body.appendChild(tooltipDiv);
+tooltipDiv.innerText = translateWord;
+
+function getSelectionTopLeft() {
+    var sel = document.selection, range, rect;
+    var x = 0, y = 0;
+    if (sel) {
+        if (sel.type != "Control") {
+            range = sel.createRange();
+            range.collapse(true);
+            x = range.boundingLeft;
+            y = range.boundingTop;
+        }
+    } else if (window.getSelection) {
+        sel = window.getSelection();
+        if (sel.rangeCount) {
+            range = sel.getRangeAt(0).cloneRange();
+            if (range.getClientRects) {
+                range.collapse(true);
+                if (range.getClientRects().length>0){
+                    rect = range.getClientRects()[0];
+                    x = rect.left;
+                    y = rect.top;
+                }
+            }
+            // Fall back to inserting a temporary element
+            if (x == 0 && y == 0) {
+                var span = document.createElement("span");
+                if (span.getClientRects) {
+                    // Ensure span has dimensions and position by
+                    // adding a zero-width space character
+                    span.appendChild( document.createTextNode("\u200b") );
+                    range.insertNode(span);
+                    rect = span.getClientRects()[0];
+                    x = rect.left;
+                    y = rect.top;
+                    var spanParent = span.parentNode;
+                    spanParent.removeChild(span);
+
+                    // Glue any broken text nodes back together
+                    spanParent.normalize();
+                }
+            }
+        }
+    }
+    return { x: x, y: y };
+}            
