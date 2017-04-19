487749b355a32da542c9d30e345f026fcb4f8cc6
nilBora <brdnlsrg@gmail.com>
2017-04-19 22:24:37 +0300

    comment

--- static/js/tooltip.js
+++ static/js/tooltip.js
@@ -10,11 +10,10 @@ var markSelection = (function() {
         var sel, range;
 
         if (document.selection && document.selection.createRange) {
-            // Clone the TextRange and collapse
+
             range = document.selection.createRange().duplicate();
             range.collapse(false);
 
-            // Create the marker element containing a single invisible character by creating literal HTML and insert it
             range.pasteHTML('<span id="' + markerId + '" style="position: relative;">' + markerTextCharEntity + '</span>');
             markerEl = document.getElementById(markerId);
         } else if (window.getSelection) {
@@ -23,12 +22,9 @@ var markSelection = (function() {
             if (sel.getRangeAt) {
                 range = sel.getRangeAt(0).cloneRange();
             } else {
-                // Older WebKit doesn't have getRangeAt
                 range.setStart(sel.anchorNode, sel.anchorOffset);
                 range.setEnd(sel.focusNode, sel.focusOffset);
 
-                // Handle the case when the selection was selected backwards (from the end to the start in the
-                // document)
                 if (range.collapsed !== sel.isCollapsed) {
                     range.setStart(sel.focusNode, sel.focusOffset);
                     range.setEnd(sel.anchorNode, sel.anchorOffset);
@@ -50,38 +46,65 @@ var markSelection = (function() {
                 selectionEl = document.createElement("div");
                 selectionEl.style.border = "solid darkblue 1px";
                 selectionEl.style.backgroundColor = "lightgoldenrodyellow";
-                selectionEl.innerHTML = "&lt;- selection";
+                selectionEl.innerHTML = "&lt;- "+translateWord;
                 selectionEl.style.position = "absolute";
 
                 document.body.appendChild(selectionEl);
             }
 
-            // Find markerEl position http://www.quirksmode.org/js/findpos.html
         var obj = markerEl;
         var left = 0, top = 0;
         do {
             left += obj.offsetLeft;
             top += obj.offsetTop;
         } while (obj = obj.offsetParent);
-
-            // Move the button into place.
-            // Substitute your jQuery stuff in here
+            var withHeight = getSelectionDimensions();
+            top = top - withHeight.height;
+            left = left - withHeight.width;
             selectionEl.style.left = left + "px";
             selectionEl.style.top = top + "px";
 
             markerEl.parentNode.removeChild(markerEl);
         }
     };
-})();
+})(translateWord);
 
 
 var positions =  getSelectionTopLeft();
-console.log(markSelection());
+markSelection(translateWord);
+console.log(getSelectionDimensions());
+
+function getSelectionDimensions() {
+    var sel = document.selection, range;
+    var width = 0, height = 0;
+    if (sel) {
+        if (sel.type != "Control") {
+            range = sel.createRange();
+            width = range.boundingWidth;
+            height = range.boundingHeight;
+        }
+    } else if (window.getSelection) {
+        sel = window.getSelection();
+        if (sel.rangeCount) {
+            range = sel.getRangeAt(0).cloneRange();
+            if (range.getBoundingClientRect) {
+                var rect = range.getBoundingClientRect();
+                width = rect.right - rect.left;
+                height = rect.bottom - rect.top;
+            }
+        }
+    }
+    return { width: width , height: height };
+}
+
+
+
+
 var tooltipDiv = document.createElement("div");
 var style = 'left:'+positions.x+'px;bottom:'+positions.y+'px;';
 tooltipDiv.setAttribute('style', style);
 tooltipDiv.setAttribute('class', 'tooltip');
-document.body.appendChild(tooltipDiv);
+//document.body.appendChild(tooltipDiv);
 tooltipDiv.innerText = translateWord;
 
 function getSelectionTopLeft() {
